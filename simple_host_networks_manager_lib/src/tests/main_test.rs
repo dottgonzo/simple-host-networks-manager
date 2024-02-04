95894a127a44c6a16111a84fe8f1d6948fa97f3a
dottg0n20 <dev@dariocaruso.info>
2024-02-04 18:14:34 +0100

    Refactor tests for networking interfaces

--- simple_host_networks_manager_lib/src/tests/main_test.rs
+++ simple_host_networks_manager_lib/src/tests/main_test.rs
@@ -1,53 +1,34 @@
 #[cfg(test)]
 mod tests {
 
-
-use default_net::interface;
-
-use crate::*;
-
+    use crate::*;
 
     #[tokio::test]
     async fn get_all_networking_phisical_interfaces_test() {
-
-        let c=get_all_networking_phisical_interfaces().await;
-
+        let c = get_all_networking_phisical_interfaces().await;
 
         assert!(c.is_ok());
-        let interfaces=c.unwrap();
+        let interfaces = c.unwrap();
 
-        println!("{:?}",interfaces.len());
-
-
-        
+        println!("{:?}", interfaces.len());
     }
     #[tokio::test]
 
     async fn get_wifi_networks_test() {
-
-        let c=get_wifi_networks().await;
-
+        let c = get_wifi_networks().await;
 
         assert!(c.is_ok());
-        let interfaces=c.unwrap();
+        let interfaces = c.unwrap();
 
-        println!("{:?}",interfaces.len());
-
-
-        
+        println!("{:?}", interfaces.len());
     }
     #[tokio::test]
 
     async fn get_routes_networks_test() {
-
-        let r=get_routes().await;
-
+        let r = get_routes().await;
 
         assert!(r.is_ok());
 
-        println!("{:?}",r);
-
-
-        
+        println!("{:?}", r);
     }
 }
